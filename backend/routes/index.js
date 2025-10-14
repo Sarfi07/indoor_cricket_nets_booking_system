1aba4a4758d40a1b24a666fad250171457dcbcca
Sarfaroj Ansari <sarfarosha66@gmail.com>
2025-10-14 10:56:50 +0530

    Initial Setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(201).json({ message: "Hello World!" });
+  res.status(201).json({ message: "Hello World! Welcome to Nexa X Online Booking System Backend" });
 });
 
 export default router;
